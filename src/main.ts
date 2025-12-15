508937ca32367de5537e2134c9a8ebf0445f81c3
lyka-ed <lykaedem11@gmail.com>
2025-12-15 01:45:12 +0100

    main entry file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ZodFilter } from './utils/filter/zod-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+  app.useGlobalFilters(new ZodFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Scissor API')
+    .setDescription('The Scissor URL Shortener API description')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
