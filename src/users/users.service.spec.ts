5ffd9faecc97fb77b7f69951fe926699a3ae335b
lyka-ed <lykaedem11@gmail.com>
2025-12-15 01:29:03 +0100

    feat(test): user unit test file

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,39 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from './users.service';
+import { PrismaService } from '../../prisma/prisma.service';
+import { EmailService } from '../utils/email/email.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+
+describe('UsersService', () => {
+  let service: UsersService;
+  let prisma: PrismaService;
+
+  const mockPrisma = {
+    user: {
+      findUnique: jest.fn(),
+      create: jest.fn(),
+    },
+  };
+  const mockEmail = { sendOtp: jest.fn() };
+  const mockCache = { get: jest.fn(), set: jest.fn() };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        { provide: PrismaService, useValue: mockPrisma },
+        { provide: EmailService, useValue: mockEmail },
+        { provide: CACHE_MANAGER, useValue: mockCache },
+      ],
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  // Add more tests here, e.g., 'should create a user'
+});
