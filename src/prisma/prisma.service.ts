f0436e509ee81d7a4b47a7fdfb6170026827b573
lyka-ed <lykaedem11@gmail.com>
2025-12-24 11:34:24 +0100

    feat(prisma) : prisma module moved to src folder

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,37 @@
+// import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+// import { PrismaClient } from '@prisma/client';
+
+// @Injectable()
+// export class PrismaService
+//   extends PrismaClient
+//   implements OnModuleInit, OnModuleDestroy
+// {
+//   async onModuleInit() {
+//     await this.$connect();
+//   }
+
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//   }
+// }
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const url = process.env.DATABASE_URL;
+
+    if (!url) {
+      throw new Error('DATABASE_URL is not defined in environment variables');
+    }
+
+    super({
+      datasourceUrl: url,
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
