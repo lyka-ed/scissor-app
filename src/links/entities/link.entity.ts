94cd89098c6066512ec71fbe8adf591685e8ed9c
lyka-ed <lykaedem11@gmail.com>
2026-01-11 15:56:47 +0100

    feat(link): create Link entity for response formatting and QR handling"

--- src/links/entities/link.entity.ts
+++ src/links/entities/link.entity.ts
@@ -1,12 +1,22 @@
+import { Link } from '@prisma/client';
+
 export class LinkEntity {
   id: number;
   originalUrl: string;
   shortUrl: string;
+  fullShortUrl: string;
   qrCode?: string;
   clicks: number;
   createdAt: Date;
 
-  constructor(partial: Partial<LinkEntity>) {
+  constructor(partial: Partial<Link>, qrCodeData?: string) {
     Object.assign(this, partial);
+
+    const baseUrl = process.env.APP_URL || 'http://localhost:3000';
+    this.fullShortUrl = `${baseUrl}/${this.shortUrl}`;
+
+    if (qrCodeData) {
+      this.qrCode = qrCodeData;
+    }
   }
 }
