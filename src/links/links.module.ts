ec93ee60656bb15bfffe688a512df40bf6581a07
lyka-ed <lykaedem11@gmail.com>
2026-01-11 17:25:30 +0100

    feat(link module): cache module added

--- src/links/links.module.ts
+++ src/links/links.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LinksService } from './links.service';
 import { LinksController } from './links.controller';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+    }),
+  ],
   controllers: [LinksController],
   providers: [LinksService],
 })
